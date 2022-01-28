1191220455272f6c357dd56e5fd6875783e5fa34
bhavimann1 <98620035+bhavimann1@users.noreply.github.com>
2022-01-28 16:40:41 -0500

    Update CameraController.cs

--- Scripts/Core/CameraController.cs
+++ Scripts/Core/CameraController.cs
@@ -4,13 +4,13 @@ public class CameraController : MonoBehaviour
 {
     //Room camera
     [SerializeField] private float speed;
-    private float currentPosX;
+    private float currentPosX; //to tell which postion the camera has to go 
     private Vector3 velocity = Vector3.zero;
 
     //Follow player
-    [SerializeField] private Transform player;
-    [SerializeField] private float aheadDistance;
-    [SerializeField] private float cameraSpeed;
+    [SerializeField] private Transform player; //Refrence to the object the camera is following 
+    [SerializeField] private float aheadDistance; //How far the camera allows you to look fowrad 
+    [SerializeField] private float cameraSpeed; //Speed of the camera 
     private float lookAhead;
 
     private void Update()
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour
         //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
-    public void MoveToNewRoom(Transform _newRoom)
+    public void MoveToNewRoom(Transform _newRoom) //Moves camera to the room the player is in 
     {
         currentPosX = _newRoom.position.x;
     }
