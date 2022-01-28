363f5010bddfdd3ea6a98413e5f975677a8bfb6f
bhavimann1 <98620035+bhavimann1@users.noreply.github.com>
2022-01-28 16:52:24 -0500

    Update SoundManager.cs

--- Scripts/Core/SoundManager.cs
+++ Scripts/Core/SoundManager.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance { get; private set; }
+    public static SoundManager instance { get; private set; } // ensures it will be stored in the memory as the only SoundManager instance 
 
-    private AudioSource source;
+    private AudioSource source; //Access the audio component to play the sound
 
 
     private void Awake()
     {
-        source = GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>(); //To assign the variable 
 
         // Keep this object even when we go to new scene
         if (instance == null)
@@ -25,7 +25,7 @@ public class SoundManager : MonoBehaviour
             Destroy(gameObject);
     }
 
-    public void PlaySound (AudioClip _sound)
+    public void PlaySound (AudioClip _sound) //takes in audio clip
     {
         source.PlayOneShot (_sound);
     }
