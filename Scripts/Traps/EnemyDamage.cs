2793ed050c54df0a9c00eff213960c98b4b2064e
RyanICS2O0 <55104645+RyanICS2O0@users.noreply.github.com>
2022-01-30 00:20:19 -0500

    Update EnemyDamage.cs

--- Scripts/Traps/EnemyDamage.cs
+++ Scripts/Traps/EnemyDamage.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    [SerializeField] protected float damage;
+    [SerializeField] protected float damage;//makes private float damage 
 
     protected void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player")//Checks if trap has collided with player
 
-            collision.GetComponent<Health>().TakeDamage(damage);
+            collision.GetComponent<Health>().TakeDamage(damage);//accesses health and decreases it
     }
 }
